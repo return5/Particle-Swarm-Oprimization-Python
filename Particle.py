6a53ead0cc90c821dbd421880e6d09aa551dc822
chris <44552743+return5@users.noreply.github.com>
2020-03-23 17:36:10 -0500

    Update Particle.py

--- Particle.py
+++ Particle.py
@@ -1,5 +1,7 @@
-#particle class for particle swarm optimization
-#class is for each individual particle which makes up the particle swarm. 
+
+"""particle class for particle swarm optimization"""
+"""class is for each individual particle which makes up the particle swarm. """
+
 
 # Copyright (C) <2020>  <github username: return5>
 
@@ -18,7 +20,9 @@
 
 import random
 
+
 class Particle:
+    """class for making particle objects as part of a swarm."""
     C_1 = 1.5        #cognitive coefficient. 
     C_2 = 1.5        #social coefficient
     INERTIAL_W = .8  #interia coefficient
@@ -26,6 +30,7 @@ class Particle:
     MAX_X = 100      #maximum possible value for x to take.
 
     def __init__(self):
+        """init particle object."""
         self.current_x = random.uniform(self.MIN_X,self.MAX_X)  #current x value for particle.initialize to a random number within range of MIN_X and MAX_X
         self.best_x = self.current_x                 #current best x value the particle has found
         self.current_result = 100                    #result of plugging in current x into the equation
@@ -33,33 +38,36 @@ class Particle:
         self.best_result = self.current_result       #best result particle has found
         self.velocity = random.uniform(-5,5)         #how far the particle should move per iteration. initalize to random value
 
-#if current x gives a better result than best x then set best to current
     def checkBestResult(self):
+        """if current x gives a better result than best x then set best to current."""
         if(abs(self.current_result) < abs(self.best_result)):
             self.best_result = self.current_result
             self.best_x = self.current_x
 
-    #attempt to solve the equation with the current_x for this particle. set the current result to the esult of the equation
-    #equation here is 3x^3 -x^2 + 7x + 3891.3369
-    #correct x for this equation is 4.2...
     def solveEquation(self):
+         """attempt to solve the equation with the current_x for this particle. set the current result to the esult of the equation
+        equation here is 3x^3 -x^2 + 7x + 3891.3369
+        correct x for this equation is 4.2...
         self.current_result = (3*self.current_x**3) - (self.current_x**2) +( 7*self.current_x) - 234.024
+        """
 
-    #prints the relevant information for that particle
-    def printInfo(self):
+    def printInfo(self):    
+        """prints the relevant information for that particle"""
         print("x: " + str(self.current_x) + " best_result: " + str(self.best_result) + " velocity: " + str(self.velocity))
 
-    #get new x value for the particle. checks to make sure the new x value is inside the range of MIN_x and MAX_X
     def getNewX(self):
+        """get new x value for the particle. checks to make sure the new x value is inside the range of MIN_x and MAX_X"""
         self.current_x = self.current_x + self.velocity
         if(self.current_x < self.MIN_X):
             self.current_x = self.MIN_X
         elif(self.current_x > self.MAX_X):
             self.current_x = self.MAX_X
 
-    #get new velocity for the particle. 
-    #first part of algorithim is the weight of the current velocity
-    #second part is how far the current result is from best result weighted by the cognitive coefficient with some randomness thrown in
-    #third part is how far current best x is from global x  weighted by the social coefficient with some randomness thrown in.
+
     def getNewVelocity(self,GLOBAL_BEST_X):
+        """get new velocity for the particle. 
+        first part of algorithim is the weight of the current velocity
+        second part is how far the current result is from best result weighted by the cognitive coefficient with some randomness thrown in
+        third part is how far current best x is from global x  weighted by the social coefficient with some randomness thrown in.
+        """
         self.velocity =  (self.INERTIAL_W * self.velocity) + (self.C_1 * random.uniform(0,2) * (self.best_x - self.current_x)) + (self.C_2 * random.uniform(0,2) * (GLOBAL_BEST_X - self.current_x))
