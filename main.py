adbbcbf53eb94233480dcd28659592ac3f74562e
chris <44552743+return5@users.noreply.github.com>
2020-03-23 15:34:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+
+#program to find roots of an equation using particle swarm optimization algorithim. 
+#written in python3
+
+# Copyright (C) <2020>  <github username: return5>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import particle
+import random
+
+SWARM = {}               #swarm is list of particle objects
+GLOBAL_BEST_X = 0        #the best X so far found. 
+GLOBAL_BEST_RESULT = 0   #the best result so far found. 
+
+
+#make each individual particle for the swarm
+def makeParticles():
+    for i in range(100):    #make 100 particles for the swarm
+        SWARM[i] = particle.Particle()
+     
+#if a particle gets a better result then set global best to that new result
+def checkGlobalBestResult(particle):
+    global GLOBAL_BEST_X,GLOBAL_BEST_RESULT                   #tell python these are global variables
+    if(abs(particle.best_result) < abs(GLOBAL_BEST_RESULT)):  #check particle's current result against the global best result
+        print("\nnew best X "   + str(particle.best_x))
+        print("new best result is " + str(particle.best_result) + "\n")
+        GLOBAL_BEST_X = particle.best_x
+        GLOBAL_BEST_RESULT = particle.best_result
+
+def particleSwarm():
+    limit = 20000                                            #limit number of total possible iterations to prevent a possible infinite loop
+    while abs(GLOBAL_BEST_RESULT) > 0.000001 and limit > 0:   #while root hasnt been found and while limit hasn't been reached
+        for i in SWARM: #for each particle in swarm
+            SWARM[i].getNewVelocity(GLOBAL_BEST_X)    #get new velocity for this particle
+            SWARM[i].getNewX()                        #get a new x value for this particle
+            SWARM[i].solveEquation()                  #try to sovle the equation with the x value of this particle
+            SWARM[i].checkBestResult()                #check the current result of the particle with its best result
+            #SWARM[i].printInfo()                      #prints information about particle for each iteration
+            checkGlobalBestResult(SWARM[i])           #check particles best result against global best result
+        limit -= 1                                    #decrement limit by 1 for each iteration of the loop
+        print("\nglobal best x is: " + str(GLOBAL_BEST_X))          #the global best x for this iteration
+        print("global best result is: " + str(GLOBAL_BEST_RESULT))  #the global best result for this iteration
+        print("limit is: " + str(limit) + "\n")
+
+
+random.seed()                                  #seed the random number generator
+makeParticles()                                #make the particles which make up the swarm
+GLOBAL_BEST_X = SWARM[0].current_x             #initalize global best x to first particle's x
+GLOBAL_BEST_RESULT = SWARM[0].current_result   #initalize global best result to first particle's result
+particleSwarm()                                #start process of particle swarm optimization
+print("best x is : " + str(GLOBAL_BEST_X))
+print("best result is: " + str(GLOBAL_BEST_RESULT))
---
+++ particle.py
@@ -0,0 +1,66 @@
+#particle class for particle swarm optimization
+#class is for each individual particle which makes up the particle swarm. 
+
+# Copyright (C) <2020>  <github username: return5>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import random
+import subprocess
+
+class Particle:
+    C_1 = 1.5        #cognitive coefficient. 
+    C_2 = 1.5        #social coefficient
+    INERTIAL_W = .8  #interia coefficient
+    MIN_X = -100     #minimum possible value for x to take
+    MAX_X = 100      #maximum possible value for x to take.
+
+    def __init__(self):
+        self.current_x = random.uniform(self.MIN_X,self.MAX_X)  #current x value for particle.initialize to a random number within range of MIN_X and MAX_X
+        self.best_x = self.current_x                 #current best x value the particle has found
+        self.current_result = 100                    #result of plugging in current x into the equation
+        self.solveEquation()                         #solve equation with current x to get true current result
+        self.best_result = self.current_result       #best result particle has found
+        self.velocity = random.uniform(-5,5)         #how far the particle should move per iteration. initalize to random value
+
+#if current x gives a better result than best x then set best to current
+    def checkBestResult(self):
+        if(abs(self.current_result) < abs(self.best_result)):
+            self.best_result = self.current_result
+            self.best_x = self.current_x
+
+    #attempt to solve the equation with the current_x for this particle. set the current result to the esult of the equation
+    #equation here is 3x^3 -x^2 + 7x + 3891.3369
+    #correct x for this equation is 4.2...
+    def solveEquation(self):
+        self.current_result = (3*self.current_x**3) - (self.current_x**2) +( 7*self.current_x) - 234.024
+
+    #prints the relevant information for that particle
+    def printInfo(self):
+        print("x: " + str(self.current_x) + " best_result: " + str(self.best_result) + " velocity: " + str(self.velocity))
+
+    #get new x value for the particle. checks to make sure the new x value is inside the range of MIN_x and MAX_X
+    def getNewX(self):
+        self.current_x = self.current_x + self.velocity
+        if(self.current_x < self.MIN_X):
+            self.current_x = self.MIN_X
+        elif(self.current_x > self.MAX_X):
+            self.current_x = self.MAX_X
+
+    #get new velocity for the particle. 
+    #first part of algorithim is the weight of the current velocity
+    #second part is how far the current result is from best result weighted by the cognitive coefficient with some randomness thrown in
+    #third part is how far current best x is from global x  weighted by the social coefficient with some randomness thrown in.
+    def getNewVelocity(self,GLOBAL_BEST_X):
+        self.velocity =  (self.INERTIAL_W * self.velocity) + (self.C_1 * random.uniform(0,2) * (self.best_x - self.current_x)) + (self.C_2 * random.uniform(0,2) * (GLOBAL_BEST_X - self.current_x))
